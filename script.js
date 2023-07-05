dd27f7cb1a17f59c30712d05d8617025a1212ac7
Garlin Gilchrist II <github@garlin.org>
2023-07-04 22:52:51 -0700

    Update script.js
    Attempt 3 rewrite

--- script.js
+++ script.js
@@ -1,110 +1,151 @@
-// Timer 1 Variables
-let timer1Running = false;
+// Timer 1 - Fifty-Ten
+const timer1StartButton = document.getElementById('timer1-start');
+const timer1ResetButton = document.getElementById('timer1-reset');
+const timer1Label = document.getElementById('timer1-label');
+const timer1Time = document.getElementById('timer1-time');
+
 let timer1Interval;
-let timer1Time = 0;
-let timer1LabelIndex = 0;
-const timer1Labels = [
-  "Fifty-Ten",
-  "Countdown",
-  "Round 1, Exercise 1, Rest",
-  "Round 1, Exercise 2, Rest",
-  "Round 1, Exercise 3, Rest",
-  "Round 1, Exercise 4, Rest",
-  "Round 2, Exercise 1, Rest",
-  "Round 2, Exercise 2, Rest",
-  "Round 2, Exercise 3, Rest",
-  "Round 2, Exercise 4, Rest",
-  "Round 3, Exercise 1, Rest",
-  "Round 3, Exercise 2, Rest",
-  "Round 3, Exercise 3, Rest",
-  "Round 3, Exercise 4, Rest",
-  "Round 4, Exercise 1, Rest",
-  "Round 4, Exercise 2, Rest",
-  "Round 4, Exercise 3, Rest",
-  "Round 4, Exercise 4, Rest",
-  "End"
-];
+let timer1TimeLeft = 10;
+let timer1Round = 0;
+let timer1State = 'paused';
 
-// Timer 2 Variables
-let timer2Running = false;
+function startTimer1() {
+  if (timer1State === 'paused') {
+    timer1State = 'running';
+    timer1StartButton.textContent = 'Pause';
+    timer1Interval = setInterval(updateTimer1, 1000);
+  } else {
+    timer1State = 'paused';
+    timer1StartButton.textContent = 'Resume';
+    clearInterval(timer1Interval);
+  }
+}
+
+function resetTimer1() {
+  clearInterval(timer1Interval);
+  timer1State = 'paused';
+  timer1StartButton.textContent = 'Start';
+  timer1Label.textContent = 'Countdown';
+  timer1Time.textContent = '10';
+  timer1TimeLeft = 10;
+  timer1Round = 0;
+}
+
+function updateTimer1() {
+  if (timer1TimeLeft === 0) {
+    timer1Round++;
+    if (timer1Round > 16) {
+      clearInterval(timer1Interval);
+      timer1State = 'paused';
+      timer1StartButton.textContent = 'Start';
+      timer1Label.textContent = 'End';
+      return;
+    }
+    if (timer1Round === 1) {
+      timer1Label.textContent = 'Round 1, Exercise 1, Rest';
+    } else {
+      timer1Label.textContent = `Round ${timer1Round}, Exercise ${timer1Round}, Rest`;
+    }
+    timer1TimeLeft = 60;
+  }
+
+  timer1TimeLeft--;
+  timer1Time.textContent = timer1TimeLeft;
+}
+
+timer1StartButton.addEventListener('click', startTimer1);
+timer1ResetButton.addEventListener('click', resetTimer1);
+
+
+// Timer 2 - Tabata
+const timer2StartButton = document.getElementById('timer2-start');
+const timer2ResetButton = document.getElementById('timer2-reset');
+const timer2Label = document.getElementById('timer2-label');
+const timer2Time = document.getElementById('timer2-time');
+
 let timer2Interval;
-let timer2Time = 0;
-let timer2LabelIndex = 0;
-const timer2Labels = [
-  "Tabata",
-  "Countdown",
-  "Exercise 1, Set 1, Rest",
-  "Exercise 1, Set 2, Rest",
-  "Exercise 1, Set 3, Rest",
-  "Exercise 1, Set 4, Rest",
-  "Exercise 1, Set 5, Rest",
-  "Exercise 1, Set 6, Rest",
-  "Exercise 1, Set 7, Rest",
-  "Exercise 1, Set 8, Rest",
-  "Exercise 2, Set 1, Rest",
-  "Exercise 2, Set 2, Rest",
-  "Exercise 2, Set 3, Rest",
-  "Exercise 2, Set 4, Rest",
-  "Exercise 2, Set 5, Rest",
-  "Exercise 2, Set 6, Rest",
-  "Exercise 2, Set 7, Rest",
-  "Exercise 2, Set 8, Rest",
-  "Exercise 3, Set 1, Rest",
-  "Exercise 3, Set 2, Rest",
-  "Exercise 3, Set 3, Rest",
-  "Exercise 3, Set 4, Rest",
-  "Exercise 3, Set 5, Rest",
-  "Exercise 3, Set 6, Rest",
-  "Exercise 3, Set 7, Rest",
-  "Exercise 3, Set 8, Rest",
-  "Exercise 4, Set 1, Rest",
-  "Exercise 4, Set 2, Rest",
-  "Exercise 4, Set 3, Rest",
-  "Exercise 4, Set 4, Rest",
-  "Exercise 4, Set 5, Rest",
-  "Exercise 4, Set 6, Rest",
-  "Exercise 4, Set 7, Rest",
-  "Exercise 4, Set 8, Rest",
-  "End"
-];
+let timer2TimeLeft = 10;
+let timer2Set = 0;
+let timer2State = 'paused';
 
-// Timer 3 Variables
-let timer3Running = false;
+function startTimer2() {
+  if (timer2State === 'paused') {
+    timer2State = 'running';
+    timer2StartButton.textContent = 'Pause';
+    timer2Interval = setInterval(updateTimer2, 1000);
+  } else {
+    timer2State = 'paused';
+    timer2StartButton.textContent = 'Resume';
+    clearInterval(timer2Interval);
+  }
+}
+
+function resetTimer2() {
+  clearInterval(timer2Interval);
+  timer2State = 'paused';
+  timer2StartButton.textContent = 'Start';
+  timer2Label.textContent = 'Countdown';
+  timer2Time.textContent = '10';
+  timer2TimeLeft = 10;
+  timer2Set = 0;
+}
+
+function updateTimer2() {
+  if (timer2TimeLeft === 0) {
+    timer2Set++;
+    if (timer2Set > 32) {
+      clearInterval(timer2Interval);
+      timer2State = 'paused';
+      timer2StartButton.textContent = 'Start';
+      timer2Label.textContent = 'End';
+      return;
+    }
+    const exercise = Math.floor((timer2Set - 1) / 8) + 1;
+    const set = (timer2Set - 1) % 8 + 1;
+    timer2Label.textContent = `Exercise ${exercise}, Set ${set}, Rest`;
+    timer2TimeLeft = 30;
+  }
+
+  timer2TimeLeft--;
+  timer2Time.textContent = timer2TimeLeft;
+}
+
+timer2StartButton.addEventListener('click', startTimer2);
+timer2ResetButton.addEventListener('click', resetTimer2);
+
+
+// Timer 3 - Stopwatch
+const timer3StartButton = document.getElementById('timer3-start');
+const timer3StopButton = document.getElementById('timer3-stop');
+const timer3Label = document.getElementById('timer3-label');
+
 let timer3Interval;
 let timer3Time = 0;
+let timer3State = 'stopped';
 
-
-
-// Function to show timer container and hide others
-function showTimer(timerContainerId) {
-  const timerContainers = document.getElementsByClassName("timer-container");
-  for (let i = 0; i < timerContainers.length; i++) {
-    timerContainers[i].style.display = "none";
+function startTimer3() {
+  if (timer3State === 'stopped') {
+    timer3State = 'running';
+    timer3StartButton.disabled = true;
+    timer3Interval = setInterval(updateTimer3, 1000);
   }
-  document.getElementById(timerContainerId).style.display = "block";
 }
 
-// Function to reset the page to initial state
-function resetPage() {
-  const timerContainers = document.getElementsByClassName("timer-container");
-  for (let i = 0; i < timerContainers.length; i++) {
-    timerContainers[i].style.display = "none";
+function stopTimer3() {
+  if (timer3State === 'running') {
+    timer3State = 'stopped';
+    timer3StartButton.disabled = false;
+    clearInterval(timer3Interval);
   }
-  document.getElementById("timer-buttons").style.display = "block";
 }
 
-// Timer Button Click Event Listeners
-document.getElementById("timer1-btn").addEventListener("click", function () {
-  showTimer("timer1-container");
-});
-document.getElementById("timer2-btn").addEventListener("click", function () {
-  showTimer("timer2-container");
-});
-document.getElementById("timer3-btn").addEventListener("click", function () {
-  showTimer("timer3-container");
-});
+function updateTimer3() {
+  timer3Time++;
+  const seconds = timer3Time % 60;
+  const minutes = Math.floor(timer3Time / 60) % 60;
+  const hours = Math.floor(timer3Time / 3600);
+  timer3Label.textContent = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
 
-// New Round Button Click Event Listener
-document.getElementById("new-round-btn").addEventListener("click", function () {
-  resetPage();
-});
+timer3StartButton.addEventListener('click', startTimer3);
+timer3StopButton.addEventListener('click', stopTimer3);
